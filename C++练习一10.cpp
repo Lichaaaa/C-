c5ce9d007f2968b6cb27bdd1ecb2321ab7f74e5c
Lichaaaa <98018180+Lichaaaa@users.noreply.github.com>
2022-01-21 17:55:20 +0800

    Add files via upload

---
+++ C++练习一10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//转义字符
+	//换行符\n
+	cout << "hello world\n";
+	//反斜杠 \\
+	//目的是单纯的输出一个反斜杠;\
+	cout <<"\\"<<end1;
+	//水平制表符 \t
+	cout << "a\thello world\n";
+	cout << "aa\thello world\n";
+	cout << "aaa\thello world\n";
+	//a hello world
+	//aa hello world
+	//aaa hello world
+	system("pause");
+}
