d7a70b58d012dc24a679df36e0b72c2f8c1b0b4a
Lichaaaa <98018180+Lichaaaa@users.noreply.github.com>
2022-02-18 21:13:48 +0800

    Add files via upload

---
+++ C++练习一25.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class Phone
+{
+public:
+	Phone(string name)
+	{
+		m_PhoneName = name;
+		cout << "Phone构造" << endl;
+	}
+
+	~Phone()
+	{
+		cout << "Phone析构" << endl;
+	}
+
+	string m_PhoneName;
+
+};
+
+
+class Person
+{
+public:
+
+	Person(string name, string pName) :m_Name(name), m_Phone(pName)
+	{
+		cout << "Person构造" << endl;
+	}
+
+	~Person()
+	{
+		cout << "Person析构" << endl;
+	}
+
+	void playGame()
+	{
+		cout << m_Name << " 使用" << m_Phone.m_PhoneName << " 牌手机! " << endl;
+	}
+
+	string m_Name;
+	Phone m_Phone;
+
+};
+void test01()
+{
+	Person p("张三", "苹果X");
+	p.playGame();
+
+}
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
+
